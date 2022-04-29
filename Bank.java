70f9c623d869ce1d55ca89b2efa050f3b475da7b
Denis Kuso <kuso.cm@gmail.com>
2022-04-29 21:03:13 +0100

    Implemented functions generating new accounts and users

--- Bank.java
+++ Bank.java
@@ -1,10 +1,69 @@
 import java.util.ArrayList;
+import java.util.Random;
 public class Bank {
 	private String name;
 	
 	private ArrayList<User> users;
 	
 	private ArrayList<Account> accounts;
+	
+	/**
+	 * Generate a new universally unique ID for a user.
+	 * @return the uuid
+	 */
+	public String getNewUserUUID() {
+		String uuid;
+		Random randomGenerator = new Random();
+		int lengthOfUUID = 6;
+		boolean nonUnique;
+		int UPPERBOUND = 10;// for random number generator
+		//Generate unique number
+		do {
+			uuid = "";
+			for(int i = 0; i < lengthOfUUID; i++) {
+				uuid += ((Integer)randomGenerator.nextInt(UPPERBOUND)).toString();
+			} 
+			
+			//Check if unique
+			nonUnique = false;
+			for(User u : this.users) {
+				if(uuid.compareTo(u.getUUID()) == 0) {
+					nonUnique = true;
+					break;
+				}
+			}
+			
+		}while (nonUnique);
+		return uuid;
+	}
+	
+	public String getNewAccountUUID() {
+		String uuid;
+		Random randomGenerator = new Random();
+		int lengthOfUUID = 10;
+		boolean nonUnique;
+		int UPPERBOUND = 10;// for random number generator
+		//Generate unique number
+		do {
+			uuid = "";
+			for(int i = 0; i < lengthOfUUID; i++) {
+				uuid += ((Integer)randomGenerator.nextInt(UPPERBOUND)).toString();
+			} 
+			
+			//Check if unique
+			nonUnique = false;
+			for(Account a : this.accounts) {
+				if(uuid.compareTo(a.getUUID()) == 0) {
+					nonUnique = true;
+					break;
+				}
+			}
+			
+		}while (nonUnique);
+		return uuid;
+	}
+	
+	
 	
 
 }
