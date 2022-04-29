4b1afe30a8a7606eb5950e130c81454c17d6a0c1
Denis Kuso <kuso.cm@gmail.com>
2022-04-29 21:07:14 +0100

    Added constructor for Transaction

--- Transaction.java
+++ Transaction.java
@@ -7,4 +7,18 @@ public class Transaction {
 	private String memo;//comment
 	
 	private Account inAccount;
+	
+	public Transaction(double amount, Account inAccount) {
+		this.amount = amount;
+		this.inAccount = inAccount;
+		this.timestamp = new Date();
+		this.memo ="";
+	}
+	
+	public Transaction(double amount, String memo, Acount inAccount) {
+		
+		// init inital constructor first
+		this(amount,inAccount);
+		this.memo = memo;
+	}
 }
