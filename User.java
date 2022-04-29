40c2a886edb42484809c09296992db66a1970727
Denis Kuso <kuso.cm@gmail.com>
2022-04-29 20:01:26 +0100

    Added constuctor to User

--- User.java
+++ User.java
@@ -1,3 +1,5 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 public class User {
 	//Define attributes
@@ -14,4 +16,29 @@ public class User {
 	 * The list of accounts for this instance of user.
 	 */
 	private ArrayList<Account> accounts;
+	
+	public User(String firstName, String lastName, String pin, Bank theBank) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		
+		//md95 hashing for storage of PIN
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			this.hashedPin = md.digest(pin.getBytes());
+		} catch (NoSuchAlgorithmException e) {
+			System.err.println("Caught NoSuchAlgorithmException");
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		// get a new unique universal ID for the user
+		this.uuid = theBank.getNewUserUUID();
+		
+		// Create empty list of accounts
+		this.accounts = new ArrayList<Account>();
+		
+		System.out.printf("New user %s, %s with ID %s created.\n", lastName, firstName,this.uuid);
+		
+		
+	}
 }
