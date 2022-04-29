13b65a6db2136f1b0aff5a1221ead55facf0f498
Denis Kuso <kuso.cm@gmail.com>
2022-04-29 20:25:14 +0100

    Added constructor for Account.java

--- Account.java
+++ Account.java
@@ -2,12 +2,29 @@ import java.util.ArrayList;
 public class Account {
 	private String nameOfAccount;
 	
-	private double balance;
+	//private double balance;
 	
 	private String uuid;
 	
 	private User holder;//owner
 	
 	private ArrayList<Transaction> transactions;
+	
+	public Account(String nameOfAccount, User holder, Bank theBank) {
+		this.nameOfAccount  = nameOfAccount;
+		this.holder = holder;
+		
+		this.uuid = theBank.getNewAccountUUID();//TO BUILD later
+		
+		//initalise transactions
+		this.transactions = new ArrayList<Transaction>();
+		
+		//Add to holder and bank list
+		holder.addAccount(this);
+		theBank.addAcount(this);
+		
+		
+		
+	}
 
 }
