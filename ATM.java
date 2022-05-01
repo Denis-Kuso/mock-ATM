e8e3ff461624f117652eaf5d8caa4b7d57798701
Denis Kuso <kuso.cm@gmail.com>
2022-05-01 08:45:13 +0100

    Implemented ATM menu prompt

--- ATM.java
+++ ATM.java
@@ -13,7 +13,41 @@ public class ATM {
 		Account newAccount = new Account("Checking", someUser, theBank);
 		someUser.addAccount(newAccount);
 		theBank.addAccount(newAccount);
+		
+		User currentUser;
+		while(true) {
+			
+			// Try a succesful login
+			currentUser = ATM.mainMenuPrompt(theBank, scanner);
+			
+			// keep menu open for user to navigate
+			ATM.printUserMenu(currentUser, scanner);
+		}
 
+	}
+	
+	public static User mainMenuPrompt(Bank theBank, Scanner scanner) {
+		String userID;
+		String pin;
+		User authUser;
+		
+		// ask for login details
+		
+		do {
+			System.out.printf("\n\nWelcome %s\n\n", theBank.getName());
+			System.out.print("Enter user ID: ");
+			userID = scanner.nextLine();
+			System.out.print("Enter PIN: ");
+			pin = scanner.nextLine();
+			
+			// Is this user our customer
+			authUser = theBank.userLogin(userID, pin);
+			if(authUser == null) {
+				System.out.println("Invalid user ID and/or PIN. " + "Please try again.");
+			}
+		} while(authUser == null); // TODO reduce to, say, 3 attempts
+		
+		return authUser;
 	}
 
 }
